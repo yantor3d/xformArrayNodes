1624a42f6cb6d6f22f5877a606654674eab07b42
Ryan Porter <ryan.m.porter@gmail.com>
2017-11-12 20:30:52 -0600

    Make nodeData header to fix compiler errors on Linux

--- src/nodes/nodeData.h
+++ src/nodes/nodeData.h
@@ -8,6 +8,7 @@
 #include "../data/quatArrayData.h"
 
 #include <maya/MAngle.h>
+#include <maya/MArrayDataBuilder.h>
 #include <maya/MArrayDataHandle.h>
 #include <maya/MDataHandle.h>
 #include <maya/MDataBlock.h>
@@ -16,6 +17,7 @@
 #include <maya/MFnCompoundAttribute.h>
 #include <maya/MFnDoubleArrayData.h>
 #include <maya/MFnMatrixArrayData.h>
+#include <maya/MFnPluginData.h>
 #include <maya/MFnPointArrayData.h>
 #include <maya/MFnVectorArrayData.h>
 #include <maya/MFnData.h>
@@ -29,7 +31,25 @@
 #include <maya/MVectorArray.h>
 
 template<class T, class FN>
-std::vector<T> getMayaArray(MDataHandle &arrayHandle);
+std::vector<T> getMayaArray(MDataHandle &arrayHandle)
+{
+    std::vector<T> result;
+
+    MObject dataObj = arrayHandle.data();
+
+    if (!dataObj.isNull())
+    {
+        FN arrayData(dataObj);
+        result.resize(arrayData.length());
+
+        for (unsigned i = 0; i < arrayData.length(); i++)
+        {
+            result[i] = T(arrayData[i]);
+        }
+    }
+
+    return result;
+}
 
 template std::vector<double>  getMayaArray<double,  MFnDoubleArrayData> (MDataHandle &arrayHandle);
 template std::vector<MMatrix> getMayaArray<MMatrix, MFnMatrixArrayData> (MDataHandle &arrayHandle);
@@ -37,7 +57,29 @@ template std::vector<MPoint>  getMayaArray<MPoint,  MFnPointArrayData>  (MDataHa
 template std::vector<MVector> getMayaArray<MVector, MFnVectorArrayData> (MDataHandle &arrayHandle);
 
 template<class T, class MA, class FN>
-MStatus setMayaArray(MDataHandle &arrayHandle, std::vector<T> &values);
+MStatus setMayaArray(MDataHandle &arrayHandle, std::vector<T> &values)
+{
+    MStatus status;
+    
+    unsigned numberOfValues = (unsigned) values.size();
+    MA output(numberOfValues);
+
+    for (unsigned i = 0; i < numberOfValues; i++)
+    {
+        output[i] = values[i];
+    }
+
+    FN fnData;
+    MObject dataObj = fnData.create(output, &status);
+    CHECK_MSTATUS_AND_RETURN_IT(status);
+
+    status = arrayHandle.setMObject(dataObj);
+    CHECK_MSTATUS_AND_RETURN_IT(status);
+
+    arrayHandle.setClean();
+    
+    return MStatus::kSuccess;
+}
 
 template MStatus setMayaArray<double,  MDoubleArray, MFnDoubleArrayData>(MDataHandle &arrayHandle, std::vector<double> &values);
 template MStatus setMayaArray<MMatrix, MMatrixArray, MFnMatrixArrayData>(MDataHandle &arrayHandle, std::vector<MMatrix> &values);
@@ -45,21 +87,79 @@ template MStatus setMayaArray<MPoint,  MPointArray,  MFnPointArrayData>(MDataHan
 template MStatus setMayaArray<MVector, MVectorArray, MFnVectorArrayData>(MDataHandle &arrayHandle, std::vector<MVector> &values);
 
 template<class T, class DATA>
-std::vector<T> getUserArray(MDataHandle& arrayHandle);
+std::vector<T> getUserArray(MDataHandle& arrayHandle)
+{
+    std::vector<T> result;
+
+    MObject dataObj = arrayHandle.data();
+
+    if (!dataObj.isNull())
+    {
+        MFnPluginData fnData(dataObj);
+        DATA* userData = (DATA*) fnData.data();
+        result = userData->getArray();
+    }
+
+    return result;
+}
 
 template std::vector<MAngle>         getUserArray<MAngle, AngleArrayData>(MDataHandle& arrayHandle);
 template std::vector<MEulerRotation> getUserArray<MEulerRotation, EulerArrayData>(MDataHandle& arrayHandle);
 template std::vector<MQuaternion>    getUserArray<MQuaternion, QuatArrayData>(MDataHandle& arrayHandle);
 
 template<class T, class DATA>
-MStatus setUserArray(MDataHandle& arrayHandle, std::vector<T> &data);
+MStatus setUserArray(MDataHandle& arrayHandle, std::vector<T> &data)
+{
+    MStatus status;
+
+    MFnPluginData fnData;
+    MObject dataObj = fnData.create(DATA::TYPE_ID, &status);
+
+    DATA* userData = (DATA*) fnData.data(&status);
+    userData->setArray(data);
+
+    status = arrayHandle.setMPxData(userData);
+    CHECK_MSTATUS_AND_RETURN_IT(status);
+
+    arrayHandle.setClean();
+
+    return status;
+}
 
 template MStatus setUserArray<MAngle, AngleArrayData>(MDataHandle& arrayHandle, std::vector<MAngle> &data);
 template MStatus setUserArray<MEulerRotation, EulerArrayData>(MDataHandle& arrayHandle, std::vector<MEulerRotation> &data);
 template MStatus setUserArray<MQuaternion, QuatArrayData> (MDataHandle& arrayHandle, std::vector<MQuaternion> &data);
 
 template<class T>
-std::vector<T> getArrayElements(MArrayDataHandle& arrayHandle, T (*getElement)(MDataHandle&), unsigned size, T fillValue);
+std::vector<T> getArrayElements(MArrayDataHandle& arrayHandle, T (*getElement)(MDataHandle&), unsigned size, T fillValue)
+{
+    std::vector<T> result(size, fillValue);
+
+    unsigned numberOfInputs = (unsigned) arrayHandle.elementCount();
+
+    std::vector<T> values(numberOfInputs);
+    std::vector<unsigned> indices(numberOfInputs);
+
+    for (unsigned i = 0; i < numberOfInputs; i++)
+    {
+        MDataHandle elementHandle = arrayHandle.inputValue();
+
+        values[i] = getElement(elementHandle);
+        indices[i] = arrayHandle.elementIndex();
+
+        arrayHandle.next();
+    }
+
+    for (unsigned i = 0; i < numberOfInputs; i++)
+    {
+        if (indices[i] < size) 
+        {
+            result[indices[i]] = values[i];
+        }
+    }
+    
+    return result;
+}
 
 template std::vector<MAngle>         getArrayElements(MArrayDataHandle& arrayHandle, MAngle (*getElement)(MDataHandle&),         unsigned size,  MAngle fillValue);
 template std::vector<MEulerRotation> getArrayElements(MArrayDataHandle& arrayHandle, MEulerRotation (*getElement)(MDataHandle&), unsigned size, MEulerRotation fillValue);
@@ -68,7 +168,31 @@ template std::vector<MQuaternion>    getArrayElements(MArrayDataHandle& arrayHan
 template std::vector<MVector>        getArrayElements(MArrayDataHandle& arrayHandle, MVector (*getElement)(MDataHandle&),        unsigned size, MVector fillValue);
 
 template<class T>
-MStatus setArrayElements(MArrayDataHandle& arrayHandle, std::vector<T> &values, MStatus (*setElement)(MDataHandle&, T));
+MStatus setArrayElements(MArrayDataHandle& arrayHandle, std::vector<T> &values, MStatus (*setElement)(MDataHandle&, T))
+{ 
+    MStatus status;
+    MArrayDataBuilder outputArray = arrayHandle.builder(&status);
+    CHECK_MSTATUS_AND_RETURN_IT(status);
+
+    unsigned numberOfValues = (unsigned) values.size();
+
+    for (unsigned i = 0; i < numberOfValues; i++)
+    {
+        MDataHandle outputHandle = outputArray.addElement(i, &status);
+        CHECK_MSTATUS_AND_RETURN_IT(status);
+
+        status = setElement(outputHandle, values[i]);
+        CHECK_MSTATUS_AND_RETURN_IT(status);
+    }
+
+    status = arrayHandle.set(outputArray);
+    CHECK_MSTATUS_AND_RETURN_IT(status);
+
+    status = arrayHandle.setAllClean();
+    CHECK_MSTATUS_AND_RETURN_IT(status);
+
+    return status;
+}
 
 template MStatus setArrayElements(MArrayDataHandle& arrayHandle, std::vector<MAngle> &values,         MStatus (*setElement)(MDataHandle&, MAngle));
 template MStatus setArrayElements(MArrayDataHandle& arrayHandle, std::vector<MEulerRotation> &values, MStatus (*setElement)(MDataHandle&, MEulerRotation));
